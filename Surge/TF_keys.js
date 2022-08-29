82a9daa4393270c3ffc2a94a39aaf309ce44570c
DecoAri <66403357+DecoAri@users.noreply.github.com>
2022-08-29 11:59:05 +0800

    添加通知

--- Surge/TF_keys.js
+++ Surge/TF_keys.js
@@ -7,4 +7,9 @@ $persistentStore.write(key, 'key')
 $persistentStore.write(session_id, 'session_id')
 $persistentStore.write(session_digest, 'session_digest')
 $persistentStore.write(request_id, 'request_id')
+if $persistentStore.read('request_id') !== null {
+  $notification.post('请关闭本脚本', '信息获取成功')
+} else {
+  $notification.post('信息获取失败','请打开MITM H2开关并添加testflight.apple.com')
+}
 $done({})
