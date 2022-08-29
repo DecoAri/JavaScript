dc1a309b6966a21017b91bef8c1772f16d0cee07
DecoAri <66403357+DecoAri@users.noreply.github.com>
2022-08-29 18:11:59 +0800

    0

--- Surge/CMB_ID.js
+++
@@ -1,4 +0,0 @@
-let chnlUserId = JSON.parse($request.body).chnlUserId
-$persistentStore.write(chnlUserId, 'chnlUserId')
-console.log('若chnlUserId后有字符串则获取成功\nchnlUserId：' + $persistentStore.read('chnlUserId'))
-$done({})
--- Surge/Piao_Bak.js
+++
@@ -1,4 +0,0 @@
-let productBakNo = JSON.parse($response.body).respData.bakNo
-$persistentStore.write(productBakNo, 'productBakNo')
-console.log('若productBakNo后有字符串则获取成功\nproductBakNo：' + $persistentStore.read('productBakNo'))
-$done({})
