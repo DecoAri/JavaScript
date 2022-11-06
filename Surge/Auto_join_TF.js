ae2f67f54edd7a5853f113de46f8f070e1a4d37d
DecoAri <66403357+DecoAri@users.noreply.github.com>
2022-11-06 12:26:19 +0800

    修复不存在该TF的问题

--- Surge/Auto_join_TF.js
+++ Surge/Auto_join_TF.js
@@ -23,24 +23,33 @@ function autoPost(ID) {
   return new Promise(function(resolve) {
     $httpClient.get({url: testurl + ID,headers: header}, function(error, resp, data) {
       if (error === null) {
-        let jsonData = JSON.parse(data)
-        if (jsonData.data == null) {
-          console.log(ID + ' ' + jsonData.messages[0].message)
-          resolve();
-        } else if (jsonData.data.status == 'FULL') {
-          console.log(ID + ' ' + jsonData.data.message)
-          resolve();
-        } else {
-          $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
-            let jsonBody = JSON.parse(body)
-            $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
-						console.log(jsonBody.data.name + ' TestFlight加入成功')
-						ids = $persistentStore.read('APP_ID').split(',')
-						ids = ids.filter(ids => ids !== ID)
-						$persistentStore.write(ids.toString(),'APP_ID')
-						resolve()
-          });
-        }
+				if (resp.status == 404) {
+					ids = $persistentStore.read('APP_ID').split(',')
+					ids = ids.filter(ids => ids !== ID)
+					$persistentStore.write(ids.toString(),'APP_ID')
+					console.log(ID + ' ' + '不存在该TF，已自动删除该APP_ID')
+					$notification.post(ID, '不存在该TF', '已自动删除该APP_ID')
+					resolve()
+				} else {
+          let jsonData = JSON.parse(data)
+          if (jsonData.data == null) {
+            console.log(ID + ' ' + jsonData.messages[0].message)
+            resolve();
+          } else if (jsonData.data.status == 'FULL') {
+            console.log(ID + ' ' + jsonData.data.message)
+            resolve();
+          } else {
+            $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
+              let jsonBody = JSON.parse(body)
+              $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
+						  console.log(jsonBody.data.name + ' TestFlight加入成功')
+						  ids = $persistentStore.read('APP_ID').split(',')
+						  ids = ids.filter(ids => ids !== ID)
+						  $persistentStore.write(ids.toString(),'APP_ID')
+						  resolve()
+            });
+          }
+				}
       } else {
         if (error =='The request timed out.') {
           resolve();
