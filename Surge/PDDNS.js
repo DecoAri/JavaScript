90cb99cfba801e877bdf98866fe95c3baace890e
DecoAri <66403357+DecoAri@users.noreply.github.com>
2023-04-01 01:22:19 +0800

    pddns

---
+++ Surge/PDDNS.js
@@ -0,0 +1,39 @@
+/****
+写脚本缘由：私有DDNS脚本，由于Surge自带的sgddns无法同时解析ipv4和ipv6同时使用。由于经常换网络环境，不一定全是公网IP，可能只有公网V6，可能全部都有。所以自写一份私有ddns脚本。
+
+使用方法：在Surge中文本编辑配置文件。
+
+[Script]这个section下粘贴以下内容：
+DDNS = type=dns,script-path=DDNS.js,argument=URL=🌍&TOKEN=🔑
+[Host]下添加以下内容：
+// 随便写一个不常见域名例如
+home.mac.pddns = script:DDNS
+把你的家里的节点的server改为上面的域名： home.mac.pddns
+
+请替换argument里面的🌍和🔑
+🌍为你github私有库文件的链接
+🔑为github的personal access token。如不知道该开什么权限请全部勾选（⚠️别把token分享给别人）
+
+自动上传IP到github请访问本人的python库。暂时未写JS脚本（也不一定会写）
+****/
+
+let ddnsurl = {
+  url: getArgs().URL,
+  headers: {
+    'Authorization': 'token ' + getArgs().TOKEN
+  }
+}
+
+$httpClient.get(ddnsurl, function(error, response, data){
+  let jsonData = JSON.parse(data)
+  $done({addresses:  [`${jsonData.ipv6}`,`${jsonData.ipv4}`], ttl: 600});
+});
+
+function getArgs() {
+  return Object.fromEntries(
+    $argument
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
+}
