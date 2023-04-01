23d1cf9e1ddd8c144ff18d62482ea080301a4d48
DecoAri <66403357+DecoAri@users.noreply.github.com>
2023-04-02 00:17:17 +0800

    note

--- Surge/PDDNS.js
+++ Surge/PDDNS.js
@@ -1,6 +1,10 @@
 /****
 写脚本缘由：私有DDNS脚本，由于Surge自带的sgddns无法同时解析ipv4和ipv6同时使用。由于经常换网络环境，不一定全是公网IP，可能只有公网V6，可能全部都有。所以自写一份私有ddns脚本。
 
+⚠️需要配合python/JS自动上传本机公网IP到github后此脚本才可用
+
+⚠️请详细（一个个字）阅读
+
 使用方法：在Surge中文本编辑配置文件。
 Surge mac端需要添加规则和节点如下:
 规则：
@@ -20,10 +24,10 @@ home.mac.pddns = script:DDNS
 把你的家里的节点的server改为上面的域名： home.mac.pddns
 
 请替换argument里面的🌍和🔑
-🌍为你github私有库文件的链接
+🌍为你github私有库的dns IP文件的链接（该文件的IP由py/js自动上传）
 🔑为github的personal access token。如不知道该开什么权限请全部勾选（⚠️别把token分享给别人）
 
-自动上传IP到github请访问本库的up_pddns.js
+自动上传IP到github请访问本库的up_pddns.js或者python库
 ****/
 
 let ddnsurl = {
