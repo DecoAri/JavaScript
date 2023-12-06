b4d4a6f5ce34d7f02fe8bc66cf71eada475158f6
DecoAri <66403357+DecoAri@users.noreply.github.com>
2023-12-06 22:16:36 +0800

    Modified by Deco

--- Surge/Auto_join_TF.js
+++ Surge/Auto_join_TF.js
@@ -55,7 +55,7 @@ function autoPost(ID) {
         if (error =='The request timed out.') {
           resolve();
         } else if (error.includes("error -1012")) { 
-          $notification.post('自动加入TF', error,'请获取TF信息')
+          $notification.post('自动加入TF', error,'请获取TF信息，模块已自动关闭')
         } else {
           $notification.post('自动加入TF', error,'')
           console.log(ID + ': ' + error)
