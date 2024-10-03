7dcd73a00aebde89eea64cde4c91235e76f0b519
DecoAri <66403357+DecoAri@users.noreply.github.com>
2024-10-04 06:56:09 +0800

    Update Auto_Join_TF.js

--- Surge/Auto_Join_TF.js
+++ Surge/Auto_Join_TF.js
@@ -1,67 +1,79 @@
 !(async () => {
 ids = $persistentStore.read('APP_ID')
 if (ids == '') {
-	$notification.post('所有TF已加入完毕','模块已自动关闭','')
-	$done($httpAPI('POST', '/v1/modules', {'Auto module for JavaScripts': 'false'}))
+  $notification.post('所有TF已加入完毕','模块已自动关闭','')
+  $done($httpAPI('POST', '/v1/modules', {'Auto module for JavaScripts': 'false'}))
 } else {
   ids = ids.split(',')
-  await list(ids)
-	$done()
+  for await (const ID of ids) {
+    await autoPost(ID)
+  }
 }
+$done()
 })();
-
-async function list(ids) {
-	for (const id of ids) {
-		await autoPost(id)
-	}
-}
 
 function autoPost(ID) {
   let Key = $persistentStore.read('key')
-  let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
+  let checkStatus = 'https://testflight.apple.com/join/' + ID
+  let checkHeader = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Mobile/15E148 Safari/604.1'
+  }
+  let joinUrl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
   let header = {
     'X-Session-Id': `${$persistentStore.read('session_id')}`,
     'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
-    'X-Request-Id': `${$persistentStore.read('request_id')}`
+    'X-Request-Id': `${$persistentStore.read('request_id')}`,
+    'X-Apple-AMD-X': `${$persistentStore.read('X-Apple-AMD-X')}`,
+    'User-Agent': `${$persistentStore.read('TFUA')}`
   }
   return new Promise(function(resolve) {
-    $httpClient.get({url: testurl + ID,headers: header}, function(error, resp, data) {
-      if (error === null) {
-        let jsonData = JSON.parse(data)
-        if (jsonData.data.status == 'FULL') {
-          console.log(ID + ' ' + jsonData.data.message)
-          resolve();
-        } else {
-          $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
-            let jsonBody = JSON.parse(body)
-            $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
-            console.log(jsonBody.data.name + ' TestFlight加入成功')
-						ids = $persistentStore.read('APP_ID').split(',')
-						console.log(ids)
-						if (ids.indexOf(ID)) {
-							console.log('in ' + ID)
-							ids.splice(ids.indexOf(ID), 1)
-							ids = ids.toString()
-							$persistentStore.write(ids,'APP_ID')
-							resolve()
-						} else if (ID == $persistentStore.read('APP_ID')) {
-							console.log('=' + ID)
-							$persistentStore.write('','APP_ID')
-							resolve()
-						} else {
-							console.log('else ' + ID)
-							resolve()
-						}
+    $httpClient.get({url: checkStatus, headers: checkHeader}, function(error, resp, data) {
+      let lowerData = data.toLowerCase()
+      switch (true) {
+        case lowerData.includes("not found"):
+          ids = $persistentStore.read('APP_ID').split(',')
+          ids = ids.filter(ids => ids !== ID)
+          $persistentStore.write(ids.toString(),'APP_ID')
+          console.log(ID + ': 不存在该TF，已自动删除该APP_ID')
+          $notification.post(ID, '不存在该TF', '已自动删除该APP_ID')
+          resolve()
+          break;
+        case lowerData.includes("this beta isn't accepting any new testers right now"):
+          console.log(ID + ": This beta isn't accepting any new testers right now")
+          resolve()
+          break;
+        case lowerData.includes("this beta is full"):
+          console.log(ID + ": This beta is full")
+          resolve()
+          break;
+        case lowerData.includes("get testflight"):
+          $httpClient.post({url: joinUrl + ID + '/accept',headers: header}, function(error, resp, body) {
+            if (error === null) {
+              let appName = JSON.parse(body).data.name
+              console.log('🎉' + appName + '🎉' + ' (' + ID + '): ' + ' TestFlight加入成功')
+              $notification.post('🎉' + appName, 'TestFlight加入成功', '')
+              ids = $persistentStore.read('APP_ID').split(',')
+              ids = ids.filter(ids => ids !== ID)
+              $persistentStore.write(ids.toString(),'APP_ID')
+              resolve();
+            } else if (error =='The request timed out.') {
+              resolve();
+            } else if (error.includes("error -1012")) {
+              $notification.post('自动加入TF', error,'请获取TF账户信息，模块已自动关闭，获取成功后再自行打开模块')
+              $done($httpAPI('POST', '/v1/modules', {'Auto module for JavaScripts': 'false'}))
+              resolve();
+            } else {
+              $notification.post('自动加入TF', error,'')
+              console.log(ID + ': ' + error)
+              resolve();
+            }
           });
-        }
-      } else {
-        if (error =='The request timed out.') {
-          resolve();
-        } else {
-          $notification.post('自动加入TF', error,'')
-          console.log(ID + ' ' + error)
-          resolve();
-        }
+          break;
+        default:
+          $notification.post('其他问题，请查看日志')
+          console.log('Error: ' + error)
+          console.log(data)
+          resolve()
       }
     })
   })
