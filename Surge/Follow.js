a8f6728f81fa3d15d73d4fac8e221ae9628e70c4
DecoAri <66403357+DecoAri@users.noreply.github.com>
2024-10-09 00:14:23 +0800

    Modified by Deco. From Source Files APP

--- Surge/Follow.js
+++ Surge/Follow.js
@@ -1,6 +1,6 @@
 function getPower(callback) {
   $httpClient.get({
-    url: $persistentStore.read('Follow_ID')
+    url: $persistentStore.read('Follow_URL')
   }, function (error, resp, body) {
     let power = JSON.parse(body).data[0].dailyPowerToken / 1e18
     callback(power);  // 将获取到的 power 通过回调传递给调用者
