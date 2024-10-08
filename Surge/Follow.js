74401e8df4941ffa97cb1051426da217d5bfab66
DecoAri <66403357+DecoAri@users.noreply.github.com>
2024-10-08 23:54:14 +0800

    Modified by Deco. From Source Files APP

--- Surge/Follow.js
+++ Surge/Follow.js
@@ -26,7 +26,7 @@ function postRequest() {
       console.log("Retrying request...");
       postRequest();  // 如果code=4000，重新发起POST请求（不获取power）
     } else {
-      $notification.post('其他错误', '查看日志', ''
+      $notification.post('其他错误', '查看日志', '')
       console.log('\n'+error + '\n'+res + '\n'+data)
       console.log("Retrying request...");
       $done();  // 其他情况结束流程
